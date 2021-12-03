29cf1904a58bdcde29792dd5fd467fcfccb274e8
Rostom <rostombaccar.et@gmail.com>
2021-12-04 00:21:26 +0100

    init pseudo function update

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -2,6 +2,8 @@ package Controller;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 import Interface.*;
 import Model.*;
 import javax.swing.*;
@@ -17,6 +19,18 @@ private Interface interf;
 		return false;
 	}
 	
+	public String initPseudo() throws IOException {
+		String pseudo;
+		Scanner s = new Scanner(System.in);
+	    System.out.println("Enter nickname");
+	    pseudo=s.nextLine();
+		while(!(this.pseudoValidity(pseudo))) { 
+			System.out.println("Nickname taken: Enter another nockname");
+			pseudo=s.nextLine();  
+		}
+		System.out.println("The nickname is valid");
+		return pseudo;
+	}
 	
 	
 	public void broadcast(DatagramSocket dgramSocket,String toSend) throws IOException {
