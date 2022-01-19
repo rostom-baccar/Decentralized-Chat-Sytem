916e42ab084953b50086fc39cf28e836e605daca
Rostom Baccar <81230614+rostom-baccar@users.noreply.github.com>
2022-01-19 09:59:05 +0100

    Added ipAddress in ClientHandler constructor

--- src/main/java/Network/ClientHandler.java
+++ src/main/java/Network/ClientHandler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -19,11 +20,13 @@ public class ClientHandler extends Thread {
 	private String clientUsername; 
 	private boolean firstConnection=true;
 	private boolean canBeAdded=false;
+	private String ipAdress;
 
 
 	//Every handler of a certain client will have access to the other handlers of the other clients
 	public ClientHandler(Socket clientSocket) throws IOException{
 		this.clientSocket=clientSocket;
+		this.ipAdress=InetAddress.getLocalHost().getHostAddress();
 		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		out = new PrintWriter(clientSocket.getOutputStream(),true);
 	}
